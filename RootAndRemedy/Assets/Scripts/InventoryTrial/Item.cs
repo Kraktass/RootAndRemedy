acd4165bcfe698371f3c66aa76c99c486151757f
Kraktass <jakob.sjoholm@live.se>
2026-03-28 20:23:17 +0100

    added pickup functionality

--- RootAndRemedy/Assets/Scripts/InventoryTrial/Item.cs
+++ RootAndRemedy/Assets/Scripts/InventoryTrial/Item.cs
@@ -1,7 +1,52 @@
+
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Item : MonoBehaviour {
+public class Item : MonoBehaviour, IInteractable {
     public ItemSO item;
     public int amount;
+
+    public Inventory inventory;
+
+
+    [SerializeField] PlanterState state = PlanterState.Empty;
+    [SerializeField] Color highlightEmission = Color.green;
+    [SerializeField] float highlightIntensity = 0.5f;
+
+    Renderer _renderer;
+    Material _materialInstance;
+    Color _originalEmissionColor;
+    bool _emissionWasEnabled;
+
+    void Awake() {
+        _renderer = GetComponent<MeshRenderer>();
+        _materialInstance = _renderer.material;
+        _originalEmissionColor = _materialInstance.GetColor("_EmissionColor");
+        _emissionWasEnabled = _materialInstance.IsKeywordEnabled("_EMISSION");
+        Debug.Log(_originalEmissionColor);
+    }
+
+    public void Interact() {
+        inventory.AddItem(item, amount);
+        Destroy(gameObject);
+    }
+
+    public bool CanInteract() {
+        return true;
+    }
+    public void Highlight() {
+        _materialInstance.EnableKeyword("_EMISSION");
+        _materialInstance.SetColor("_EmissionColor", highlightEmission * highlightIntensity);
+    }
+
+    public void UnHighlight() {
+        _materialInstance.SetColor("_EmissionColor", _originalEmissionColor);
+        if (_emissionWasEnabled) {
+            _materialInstance.EnableKeyword("_EMISSION");
+        }
+        else {
+            _materialInstance.DisableKeyword("_EMISSION");
+        }
+    }
 
 }
--- RootAndRemedy/Assets/Scripts/Player/CheckInteractable.cs
+++ RootAndRemedy/Assets/Scripts/Player/CheckInteractable.cs
@@ -1,5 +1,4 @@
-using System.Collections.Generic;
-using Unity.Multiplayer.Center.Common.Analytics;
+/* using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckInteractable : MonoBehaviour {
@@ -62,11 +61,117 @@ public class CheckInteractable : MonoBehaviour {
 
     public void OnInteract() {
         var actedOn = currentInteractable;
-        if (actedOn != null) {
-            actedOn.Interact();
-            Debug.Log("You planted in: " + currentInteractable);
-        }
+        actedOn?.Interact();
         SelectNearest();
     }
 
+} */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckInteractable : MonoBehaviour {
+    IInteractable currentInteractable;
+    IInteractable lastHighlighted;
+    PlayerMovement movement;
+
+    public List<IInteractable> interactables = new();
+
+    void Awake() {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    void Update() {
+        if (movement.IsMoving && interactables.Count > 0) {
+            SelectNearest();
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.TryGetComponent<IInteractable>(out var interactable)) {
+            if (!IsValid(interactable)) return;
+            if (!interactables.Contains(interactable))
+                interactables.Add(interactable);
+
+            currentInteractable = interactable;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (!other.TryGetComponent<IInteractable>(out var interactable)) return;
+
+        interactables.Remove(interactable);
+
+        if (ReferenceEquals(interactable, currentInteractable))
+            currentInteractable = null;
+
+        if (ReferenceEquals(interactable, lastHighlighted))
+            lastHighlighted = null;
+
+        SelectNearest();
+    }
+
+    bool IsValid(IInteractable interactable) {
+        return interactable is MonoBehaviour mb && mb != null;
+    }
+
+    void SelectNearest() {
+        interactables.RemoveAll(i => !IsValid(i));
+
+        if (!IsValid(currentInteractable))
+            currentInteractable = null;
+
+        if (!IsValid(lastHighlighted))
+            lastHighlighted = null;
+
+        IInteractable nearestTarget = null;
+        Vector3 currentPosition = transform.position;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (IInteractable interactable in interactables) {
+            if (!interactable.CanInteract()) continue;
+
+            MonoBehaviour mb = (MonoBehaviour)interactable;
+            float distanceSquaredToTarget = (mb.transform.position - currentPosition).sqrMagnitude;
+
+            if (distanceSquaredToTarget < closestDistance) {
+                closestDistance = distanceSquaredToTarget;
+                nearestTarget = interactable;
+            }
+        }
+
+        if (ReferenceEquals(nearestTarget, lastHighlighted)) {
+            currentInteractable = nearestTarget;
+            return;
+        }
+
+        if (IsValid(lastHighlighted))
+            lastHighlighted.UnHighlight();
+
+        if (IsValid(nearestTarget))
+            nearestTarget.Highlight();
+
+        lastHighlighted = nearestTarget;
+        currentInteractable = nearestTarget;
+    }
+
+    public void OnInteract() {
+        var actedOn = currentInteractable;
+
+        if (!IsValid(actedOn)) return;
+
+        actedOn.Interact();
+
+        if (!IsValid(actedOn)) {
+            interactables.RemoveAll(i => !IsValid(i));
+
+            if (ReferenceEquals(actedOn, currentInteractable))
+                currentInteractable = null;
+
+            if (ReferenceEquals(actedOn, lastHighlighted))
+                lastHighlighted = null;
+        }
+
+        SelectNearest();
+    }
 }
